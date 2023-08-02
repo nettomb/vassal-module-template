2158935957cbccb47519ee8672bb149c9d4505a3
Marcello Netto <nettomb@hotmail.com>
2023-08-02 09:47:09 -0300

    Tested with dice images created in Blender. Alpha working ok. All pieces showing causing a trail to display. Uncompressed PNGs too slow to load. Compressed PNGs OK.

--- src/main/java/my_custom_component/AnimatedDice.java
+++ src/main/java/my_custom_component/AnimatedDice.java
@@ -16,26 +16,31 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public final class AnimatedDice extends AbstractBuildable implements CommandEncoder, Buildable{
     private GameModule gameModule;
-    private String buttonText = "Show Piece";
     private static final String DICE_IMAGE_FOLDER = "my_custom_component/images";
     private static int filesInFolder;
     private static final int IMAGE_SIZE = 300;
     private boolean isImageVisible;
     private JButton customButton;
-    private long FRAME_RATE;
+    private final long FRAME_RATE; // The number of MILLISECONDS between the display actions.
+    int currentFrame;
+    private ScheduledExecutorService scheduler; // Controls the frame rate of the displayed images
     private Image[] images; // to be fed with the dice images that will be drawn on the pieces
     private BasicPiece[] pieces; // pieces are added to this array to be displayed in order
-    private Map currentMap;
+    private final Map currentMap;
 
     public AnimatedDice(){
         isImageVisible = false;
         currentMap = GameModule.getGameModule().getComponentsOf(Map.class).get(0);
         gameModule = GameModule.getGameModule();
         filesInFolder = countFilesInFolder(System.getProperty("user.dir") + "/target/classes/" + DICE_IMAGE_FOLDER);
-        FRAME_RATE = 30;
+        FRAME_RATE = 1000/30;
+        currentFrame = 0;
     }
 
     public static void main(String[] args) {
@@ -49,7 +54,7 @@ public final class AnimatedDice extends AbstractBuildable implements CommandEnco
             gameModule = (GameModule) parent;
 
             // Create your button instance
-            customButton = new JButton(buttonText);
+            customButton = new JButton("Show Piece");
             customButton.setToolTipText("Displays Image on map");
 
             customButton.addActionListener(new ActionListener() {
@@ -64,7 +69,7 @@ public final class AnimatedDice extends AbstractBuildable implements CommandEnco
         }
     }
 
-    private void toggleImagesVisibility(){
+    /*private void toggleImagesVisibility(){
         if (isImageVisible){
             for (BasicPiece piece: pieces) {
                 hideImage(piece);
@@ -87,6 +92,53 @@ public final class AnimatedDice extends AbstractBuildable implements CommandEnco
             int xCoordinate = 0;
             int yCoordinate = 0;
             currentMap.placeAt(piece, new Point(xCoordinate,yCoordinate));
+        }
+    }*/
+
+    private void toggleImagesVisibility(){
+        if (isImageVisible){
+            customButton.setEnabled(false);
+            for (BasicPiece piece: pieces) {
+                hideImage(piece);
+            }
+            isImageVisible = false;
+            customButton.setText("Show Image");
+            customButton.setEnabled(true);
+        } else {
+            customButton.setEnabled(false);
+            getImages(); // We must populate the images array before calling createPieces.
+            createPieces();
+            scheduler = Executors.newSingleThreadScheduledExecutor();
+            scheduler.scheduleAtFixedRate(this::displayImage, 0, FRAME_RATE, TimeUnit.MILLISECONDS);
+        }
+    }
+
+    public void stopImageDisplay(){
+        scheduler.shutdown();
+        try{
+            if (!scheduler.awaitTermination(1, TimeUnit.SECONDS)){
+                scheduler.shutdownNow();
+                customButton.setEnabled(true);
+            }
+        } catch (InterruptedException ex){
+            scheduler.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void displayImage(){
+        if (currentMap != null){
+            if (currentFrame == pieces.length) {
+                stopImageDisplay();
+                currentFrame = 0;
+                isImageVisible = true;
+                customButton.setText("Hide Image");
+                return;
+            }
+            int xCoordinate = 0;
+            int yCoordinate = 0;
+            currentMap.placeAt(pieces[currentFrame], new Point(xCoordinate,yCoordinate));
+            currentFrame = (currentFrame + 1);
         }
     }
 
@@ -112,7 +164,7 @@ public final class AnimatedDice extends AbstractBuildable implements CommandEnco
                 }
             };
             pieces[i] = piece;
-            System.out.println("piece at slot " + i + "is named: " + piece.toString());
+            System.out.println("piece at slot " + i + "is named: " + piece);
         }
     }
 
@@ -122,9 +174,12 @@ public final class AnimatedDice extends AbstractBuildable implements CommandEnco
         System.out.println("# images: " + filesInFolder);
         for (int i = 0; i < filesInFolder; i++) {
             try {
-                URL imageURL = getClass().getResource("/" + DICE_IMAGE_FOLDER + "/dices" + i + ".png");
+                URL imageURL = getClass().getResource("/" + DICE_IMAGE_FOLDER + "/" + String.format("dices%04d", i) + ".png");
+                System.out.println(String.format("/dices%04d.png", i));
+                System.out.println("URL: " + imageURL);
                 if (imageURL != null) {
                     images[i] = (ImageIO.read(imageURL));
+                    System.out.println(images[i]);
                 } else {
                     throw new IOException("Image file not found.");
                 }
@@ -173,7 +228,7 @@ public final class AnimatedDice extends AbstractBuildable implements CommandEnco
 
     @Override
     public String getAttributeValueString(String value){
-        return new String ("");
+        return "";
     }
 
     @Override
